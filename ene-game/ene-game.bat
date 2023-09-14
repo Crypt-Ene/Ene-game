73bd83a8dcebbebf9b6418391cfd176278ad7b8c
Crypt_Ene <64547010+Crypt-Ene@users.noreply.github.com>
2023-09-14 18:49:46 +0100

    Game can now be restarted upon death

---
+++ ene-game/ene-game.bat
@@ -0,0 +1,2 @@
+"C:/Users/Tom Jr/AppData/Local/Microsoft/WindowsApps/python3.11.exe" "g:/Stuff and Things/Code/Python stuff/ene-game/ene-game.py"
+pause
--- ene-game/ene-game.py
+++ ene-game/ene-game.py
@@ -7,241 +7,244 @@ path = __file__
 path = path.replace("ene-game.py", "")
 os.chdir(path)
 
-#variables
-ScreenWidth = 800
-ScreenHeight = 600
-global CameraX, WorldPositionX, PlayerHealth, DamageTaken, DamageHealed, gameon, running, gameover, directory
-BackgroundImage = pygame.image.load("images\Background.png")
-gameon = True
-running = False 
-gameover = False
-CameraX = 0
-WorldPositionX = 0
-PlayerHealth = 3
-DamageTaken = False
-DamageHealed = False
 coloroptions = ["Blue", "Red", "Green", "Yellow", "Purple"]
 
-#sprites        
-class Player(pygame.sprite.Sprite):
-    def __init__(self, startx, starty):
-        pygame.sprite.Sprite.__init__(self)
-        while True:
-            self.ColorScheme = str(input("What color, Blue, Red, Green, Yellow or Purple: "))
-            self.ColorScheme = self.ColorScheme.capitalize()
-            if self.ColorScheme in coloroptions:
-                self.ColorScheme = self.ColorScheme + "-"
-                break
-            else:
-                print("Try Again")
-        self.image = pygame.image.load(f"images\{self.ColorScheme}ene-float-1.png")
-        self.rect = self.image.get_rect()
-        self.rect = self.rect.inflate(-5, -5)
-        self.rect.center = [(startx), starty]
-        self.imagelist = [(pygame.image.load(f"images\{self.ColorScheme}ene-float-1.png")), (pygame.image.load(f"images\{self.ColorScheme}ene-float-2.png"))]
-        self.currentframe = 0
-        self.prevkey = pygame.key.get_pressed()
-        self.speed = 0.75
-        self.minspeed = -8
-        self.maxspeed = 8
-        self.jumpspeed = 12
-        self.gravity = 0.5
-        self.animation_index = 0
-        self.animation_tick = 0
-        self.facing_left = False
-        self.h_momentum = 0
-        self.v_momentum = 0
-        self.doublejump = True
-        self.doublejumptick = 0
-        self.safepointx = startx
-        self.safepointy = starty
-
-    def check_fall(self):
-        if self.rect.y > 600:
-            global PlayerHealth, DamageTaken
-            if PlayerHealth == 1:
-                DamageTaken = True
-                return
-            global CameraX, WorldPositionX
-            CameraX = self.safepointx - WorldPositionX
-            objects.update(self)
-            WorldPositionX = self.safepointx
-            self.rect.y = self.safepointy
-            DamageTaken = True
-            self.h_momentum = 0
-
-    def check_collision(self, x, y, objects):
-        self.rect.move_ip([x, y])
-        collide = pygame.sprite.spritecollideany(self, objects)
-        self.rect.move_ip([-x,-y])
-        return collide
-
-    def player_animation(self):
-        self.image = self.imagelist[self.currentframe]
-        if self.facing_left == True:
-            self.image = pygame.transform.flip(self.image, True, False)
-
-        if self.animation_tick == 40:
-            self.animation_tick = 0
-            if self.animation_index == 0:
-                self.currentframe = 0
-                self.animation_index = 1
-            else:
-                self.currentframe = 1
-                self.animation_tick = -20
-                self.animation_index = 0
-        else:
-            self.animation_tick += 1
-
-    def update(self, objects):
-        self.player_animation()
-        key = pygame.key.get_pressed()
-        self.check_fall()
-        onground = self.check_collision(0, 1, objects)
-
-        if numpy.sign(self.h_momentum) != 0:
-            speedtype = numpy.sign(self.h_momentum)
-            if speedtype > 0:
-                self.h_momentum += -(self.speed/2)
-            else:
-                self.h_momentum += (self.speed/2)
-
-        if self.doublejumptick > 0:
-            self.doublejumptick -= 1
-
-        if key[pygame.K_LEFT] and not key[pygame.K_RIGHT]:
-            if self.h_momentum > self.minspeed:
-                self.h_momentum += -self.speed
-            self.facing_left = True
-
-        elif key[pygame.K_RIGHT] and not key[pygame.K_LEFT]:
-            if self.h_momentum < self.maxspeed:
-                self.h_momentum += self.speed
-            self.facing_left = False
-
-        if onground:
-            global WorldPositionX
-            self.safepointx = WorldPositionX
-            self.safepointy = self.rect.y
-            self.v_momentum = 0
-            self.doublejump = True
-
-        if key[pygame.K_UP] and onground:
-            self.v_momentum = -self.jumpspeed
-            self.doublejumptick = 15
-
-        elif key[pygame.K_UP] and self.doublejump and self.doublejumptick == 0:
-            self.v_momentum = -self.jumpspeed
-            self.doublejump = False
-
-        if self.v_momentum < 10 and not onground:
-            self.v_momentum += self.gravity
-
-        self.move(self.h_momentum, self.v_momentum, objects)
-
-    def move(self, x, y, objects):
-        dx = x
-        dy = y
-        i = 0
-        while self.check_collision(0, dy, objects) and i < (20):
-            dy -= numpy.sign(dy)
-            i += 1
-        i = 0
-        while self.check_collision(dx, 0, objects) and i < (20):
-            dx -= numpy.sign(dx)
-            i += 1
-        i = 0
-        global CameraX, WorldPositionX
-        CameraX = dx
-        WorldPositionX += dx
-        self.rect.move_ip([0, dy])
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-    
-class HealthBar(pygame.sprite.Sprite):
-    def __init__(self, startx, starty):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load("images\ene-health-full.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = [(startx), starty]
-        self.full = True
-
-    def damage(self):
-        self.image = pygame.image.load("images\ene-health-empty.png")
-        self.full = False
-        global PlayerHealth
-        PlayerHealth -= 1
-
-    def healed(self):
-        self.image = pygame.image.load("images\ene-health-full.png")
-        self.full = True
-        global PlayerHealth
-        PlayerHealth += 1
-
-class Health1(HealthBar):
-    def __init__(self, startx, starty):
-        HealthBar.__init__(self, startx, starty)
-
-    def update(self):
-        global DamageTaken, DamageHealed, PlayerHealth
-        if DamageTaken == True and PlayerHealth == 1:
-            self.damage()
-            DamageTaken = False
-            Gameover()
-        if DamageHealed == True:
-            self.healed()
-            DamageHealed = False
-class Health2(HealthBar):
-    def __init__(self, startx, starty):
-        HealthBar.__init__(self, startx, starty)
-
-    def update(self):
-        global DamageTaken, DamageHealed, PlayerHealth
-        
-        if DamageTaken == True and PlayerHealth == 2:
-            self.damage()
-            DamageTaken = False
-        if DamageHealed == True:
-            self.healed()
-            DamageHealed = False
-class Health3(HealthBar):
-    def __init__(self, startx, starty):
-        HealthBar.__init__(self, startx, starty)
-
-    def update(self):
-        global DamageTaken, DamageHealed, PlayerHealth
-        
-        if DamageTaken == True and PlayerHealth == 3:
-            self.damage()
-            DamageTaken = False
-        if DamageHealed == True:
-            self.healed()
-            DamageHealed = False
-
-class FloorTile(pygame.sprite.Sprite):
-    def __init__(self, startx, starty):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("images\Floor.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = [(startx), starty]
-        self.immune = True
-
-    def update(self, player):
-        self.distance = (player.rect.x - self.rect.x)
-        if self.distance < 2000 and self.distance > -2000:
-            self.immune = False
-        if self.distance > 4000 or self.distance < -10000 and not self.immune:
-            self.kill()
-        self.rect.move_ip([-CameraX, 0])
-
-def Gameover():
-    global running, gameover
-    running = False
-    gameover = True
+global ColorScheme
+while True:
+                ColorScheme = str(input("What color, Blue, Red, Green, Yellow or Purple: "))
+                ColorScheme = ColorScheme.capitalize()
+                if ColorScheme in coloroptions:
+                    ColorScheme = ColorScheme + "-"
+                    break
+                else:
+                    print("Try Again")
 
 def main():
+    #variables
+    ScreenWidth = 800
+    ScreenHeight = 600
+    global CameraX, WorldPositionX, PlayerHealth, DamageTaken, DamageHealed, gameon, running, gameover, directory
+    BackgroundImage = pygame.image.load("images\Background.png")
+    gameon = True
+    running = False 
+    gameover = False
+    CameraX = 0
+    WorldPositionX = 0
+    PlayerHealth = 3
+    DamageTaken = False
+    DamageHealed = False
+
+    #sprites        
+    class Player(pygame.sprite.Sprite):
+        def __init__(self, startx, starty):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load(f"images\{ColorScheme}ene-float-1.png")
+            self.rect = self.image.get_rect()
+            self.rect = self.rect.inflate(-5, -5)
+            self.rect.center = [(startx), starty]
+            self.imagelist = [(pygame.image.load(f"images\{ColorScheme}ene-float-1.png")), (pygame.image.load(f"images\{ColorScheme}ene-float-2.png"))]
+            self.currentframe = 0
+            self.prevkey = pygame.key.get_pressed()
+            self.speed = 0.75
+            self.minspeed = -8
+            self.maxspeed = 8
+            self.jumpspeed = 12
+            self.gravity = 0.5
+            self.animation_index = 0
+            self.animation_tick = 0
+            self.facing_left = False
+            self.h_momentum = 0
+            self.v_momentum = 0
+            self.doublejump = True
+            self.doublejumptick = 0
+            self.safepointx = startx
+            self.safepointy = starty
+
+        def check_fall(self):
+            if self.rect.y > 600:
+                global PlayerHealth, DamageTaken
+                if PlayerHealth == 1:
+                    DamageTaken = True
+                    return
+                global CameraX, WorldPositionX
+                CameraX = self.safepointx - WorldPositionX
+                objects.update(self)
+                WorldPositionX = self.safepointx
+                self.rect.y = self.safepointy
+                DamageTaken = True
+                self.h_momentum = 0
+
+        def check_collision(self, x, y, objects):
+            self.rect.move_ip([x, y])
+            collide = pygame.sprite.spritecollideany(self, objects)
+            self.rect.move_ip([-x,-y])
+            return collide
+
+        def player_animation(self):
+            self.image = self.imagelist[self.currentframe]
+            if self.facing_left == True:
+                self.image = pygame.transform.flip(self.image, True, False)
+
+            if self.animation_tick == 40:
+                self.animation_tick = 0
+                if self.animation_index == 0:
+                    self.currentframe = 0
+                    self.animation_index = 1
+                else:
+                    self.currentframe = 1
+                    self.animation_tick = -20
+                    self.animation_index = 0
+            else:
+                self.animation_tick += 1
+
+        def update(self, objects):
+            self.player_animation()
+            key = pygame.key.get_pressed()
+            self.check_fall()
+            onground = self.check_collision(0, 1, objects)
+
+            if numpy.sign(self.h_momentum) != 0:
+                speedtype = numpy.sign(self.h_momentum)
+                if speedtype > 0:
+                    self.h_momentum += -(self.speed/2)
+                else:
+                    self.h_momentum += (self.speed/2)
+
+            if self.doublejumptick > 0:
+                self.doublejumptick -= 1
+
+            if key[pygame.K_LEFT] and not key[pygame.K_RIGHT]:
+                if self.h_momentum > self.minspeed:
+                    self.h_momentum += -self.speed
+                self.facing_left = True
+
+            elif key[pygame.K_RIGHT] and not key[pygame.K_LEFT]:
+                if self.h_momentum < self.maxspeed:
+                    self.h_momentum += self.speed
+                self.facing_left = False
+
+            if onground:
+                global WorldPositionX
+                self.safepointx = WorldPositionX
+                self.safepointy = self.rect.y
+                self.v_momentum = 0
+                self.doublejump = True
+
+            if key[pygame.K_UP] and onground:
+                self.v_momentum = -self.jumpspeed
+                self.doublejumptick = 15
+
+            elif key[pygame.K_UP] and self.doublejump and self.doublejumptick == 0:
+                self.v_momentum = -self.jumpspeed
+                self.doublejump = False
+
+            if self.v_momentum < 10 and not onground:
+                self.v_momentum += self.gravity
+
+            self.move(self.h_momentum, self.v_momentum, objects)
+
+        def move(self, x, y, objects):
+            dx = x
+            dy = y
+            i = 0
+            while self.check_collision(0, dy, objects) and i < (20):
+                dy -= numpy.sign(dy)
+                i += 1
+            i = 0
+            while self.check_collision(dx, 0, objects) and i < (20):
+                dx -= numpy.sign(dx)
+                i += 1
+            i = 0
+            global CameraX, WorldPositionX
+            CameraX = dx
+            WorldPositionX += dx
+            self.rect.move_ip([0, dy])
+
+        def draw(self, screen):
+            screen.blit(self.image, self.rect)
+
+    class HealthBar(pygame.sprite.Sprite):
+        def __init__(self, startx, starty):
+            pygame.sprite.Sprite.__init__(self)
+
+            self.image = pygame.image.load("images\ene-health-full.png")
+            self.rect = self.image.get_rect()
+            self.rect.center = [(startx), starty]
+            self.full = True
+
+        def damage(self):
+            self.image = pygame.image.load("images\ene-health-empty.png")
+            self.full = False
+            global PlayerHealth
+            PlayerHealth -= 1
+
+        def healed(self):
+            self.image = pygame.image.load("images\ene-health-full.png")
+            self.full = True
+            global PlayerHealth
+            PlayerHealth += 1
+
+    class Health1(HealthBar):
+        def __init__(self, startx, starty):
+            HealthBar.__init__(self, startx, starty)
+
+        def update(self):
+            global DamageTaken, DamageHealed, PlayerHealth
+            if DamageTaken == True and PlayerHealth == 1:
+                self.damage()
+                DamageTaken = False
+                Gameover()
+            if DamageHealed == True:
+                self.healed()
+                DamageHealed = False
+    class Health2(HealthBar):
+        def __init__(self, startx, starty):
+            HealthBar.__init__(self, startx, starty)
+
+        def update(self):
+            global DamageTaken, DamageHealed, PlayerHealth
+
+            if DamageTaken == True and PlayerHealth == 2:
+                self.damage()
+                DamageTaken = False
+            if DamageHealed == True:
+                self.healed()
+                DamageHealed = False
+    class Health3(HealthBar):
+        def __init__(self, startx, starty):
+            HealthBar.__init__(self, startx, starty)
+
+        def update(self):
+            global DamageTaken, DamageHealed, PlayerHealth
+
+            if DamageTaken == True and PlayerHealth == 3:
+                self.damage()
+                DamageTaken = False
+            if DamageHealed == True:
+                self.healed()
+                DamageHealed = False
+
+    class FloorTile(pygame.sprite.Sprite):
+        def __init__(self, startx, starty):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load("images\Floor.png")
+            self.rect = self.image.get_rect()
+            self.rect.center = [(startx), starty]
+            self.immune = True
+
+        def update(self, player):
+            self.distance = (player.rect.x - self.rect.x)
+            if self.distance < 2000 and self.distance > -2000:
+                self.immune = False
+            if self.distance > 4000 or self.distance < -10000 and not self.immune:
+                self.kill()
+            self.rect.move_ip([-CameraX, 0])
+
+    def Gameover():
+        global running, gameover
+        running = False
+        gameover = True
+
     icon = pygame.image.load("images\ene-health-full.png")
     pygame.display.set_caption("Ene Game")
     pygame.display.set_icon(icon)
@@ -281,13 +284,10 @@ def main():
     nbt = 0
 
     #gameloop
-    global gameon, running
     running = True
     alpha = 0
     gameovertick = 0
-    alphaX = 20
     while gameon:
-        global gameover
         while running:
             pygame.event.pump()
             player.update(objects)
@@ -327,10 +327,9 @@ def main():
             clock.tick(60)
         if gameover == True:
             if alpha < 255:
-                if gameovertick >= alphaX:
-                    alpha += 1
+                if gameovertick > 10:
+                    alpha += 2
                     gameovertick = 0
-                    alphaX -= 2
                 else:
                     gameovertick += 1
             
@@ -344,6 +343,9 @@ def main():
                     if event.key == pygame.K_ESCAPE:
                         running = False
                         gameon = False
+
+                    if event.key == pygame.K_SPACE:
+                        return main()
             
             Gameoverimage.set_alpha(alpha)
             screen.blit(Gameoverimage, (0, 0))
